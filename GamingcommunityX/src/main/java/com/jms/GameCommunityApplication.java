7c310a7220152f171739664b3ff319b3ce66beeb
JMS <anstndwkd@naver.com>
2022-10-27 20:50:45 +0900

    public 폴더 -> _redirects 파일생성 -> /* /index.html 200 코드 추가

--- GamingcommunityX/src/main/java/com/jms/GameCommunityApplication.java
+++ GamingcommunityX/src/main/java/com/jms/GameCommunityApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class GameCommunityApplication  {
 		
 	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GameCommunityApplication.class, args);
 	}
